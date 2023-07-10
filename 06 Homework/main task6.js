da2d45d3f827c32199b75d1678c43e37e94be32d
KharkevychAndrii <136821218+KharkevychAndrii@users.noreply.github.com>
2023-07-10 23:09:45 +0300

    Update main task6.js

--- 06 Homework/main task6.js
+++ 06 Homework/main task6.js
@@ -1,16 +1,50 @@
-const colors = document.querySelectorAll('.color');
-const priceOutput = document.getElementById('outprice');
-const discountOutput = document.getElementById('discount');
-const currencyOutput = document.getElementById('currency');
+const colotBtnList = document.querySelectorAll('.color');
+const shoesList = document.querySelectorAll('.shoe');
+const bgList = document.querySelectorAll('.gradient');
+const outPrice = document.querySelector('#outprice');
 
-colors.forEach(function(color) {
-  color.addEventListener('click', function() {
-    const price = color.getAttribute('data-price');
-    const discount = color.getAttribute('data-price-discount');
-    const currency = color.getAttribute('data-price-currency');
-    
-    priceOutput.textContent = price;
-    discountOutput.textContent = discount;
-    currencyOutput.textContent = currency;
-  });
+let currentColor = 'blue'; // початковий колір
+let currentPrice = 170; // початкова ціна
+
+colotBtnList.forEach((btn) => {
+  btn.addEventListener('click', changeCard);
 });
+
+function changeCard(event) {
+  // Отримуємо вибраний колір та ціну з атрибутів елемента
+  const color = event.target.getAttribute('color');
+  const price = event.target.getAttribute('data-price');
+
+  // Змінюємо клас активного коліру
+  colotBtnList.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+  event.target.classList.add('active');
+
+  // Змінюємо зображення товару
+  shoesList.forEach((shoe) => {
+    const shoeColor = shoe.getAttribute('color');
+    shoe.classList.remove(`show`);
+    if (shoeColor === color) {
+      shoe.classList.add('show');
+    } else {
+      shoe.classList.remove('show');
+    }
+  });
+
+  // Змінюємо фоновий градієнт
+  bgList.forEach((bg) => {
+    const bgColor = bg.getAttribute('color');
+    bg.classList.remove(`first`);
+    if (bgColor === color) {
+      bg.classList.add('first');
+    } else {
+      bg.classList.remove('first');
+    }
+  });
+
+  // Оновлюємо відображення ціни товару
+  currentColor = color;
+  currentPrice = price;
+  outPrice.textContent = price;
+}
