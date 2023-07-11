58359035f637c668fa6dd7ca6bfa837985800ec5
KharkevychAndrii <136821218+KharkevychAndrii@users.noreply.github.com>
2023-07-11 12:32:37 +0300

    Update main task6.js

--- 06 Homework/main task6.js
+++ 06 Homework/main task6.js
@@ -2,9 +2,13 @@ const colotBtnList = document.querySelectorAll('.color');
 const shoesList = document.querySelectorAll('.shoe');
 const bgList = document.querySelectorAll('.gradient');
 const outPrice = document.querySelector('#outprice');
+const shoeQuantity = document.querySelectorAll(`.quantity`);
+const deliveryNeed = document.querySelectorAll(`.del`);
 
 let currentColor = 'blue'; // початковий колір
 let currentPrice = 170; // початкова ціна
+let currentQuantity = 1; // початкова кількість
+let currentDelivery = 1; 
 
 colotBtnList.forEach((btn) => {
   btn.addEventListener('click', changeCard);
@@ -35,16 +39,33 @@ function changeCard(event) {
   // Змінюємо фоновий градієнт
   bgList.forEach((bg) => {
     const bgColor = bg.getAttribute('color');
-    bg.classList.remove(`first`);
+    bg.classList.remove(`second`);
     if (bgColor === color) {
       bg.classList.add('first');
     } else {
       bg.classList.remove('first');
     }
   });
-
   // Оновлюємо відображення ціни товару
   currentColor = color;
   currentPrice = price;
-  outPrice.textContent = price;
+  outPrice.textContent = currentPrice * currentDelivery * currentQuantity
 }
+
+shoeQuantity.forEach((qnt) => {
+  qnt.addEventListener(`click`, changeQuantity);
+});
+function changeQuantity(event) {
+  const quantity = event.target.getAttribute(`quant`);
+ currentQuantity = quantity;
+ outPrice.textContent = currentPrice * currentDelivery * currentQuantity
+};
+
+deliveryNeed.forEach((del) => {
+  del.addEventListener(`click`, changeDilivery);
+});
+function changeDilivery(event) {
+const deliv = event.target.getAttribute(`del`);
+ currentDelivery = deliv;
+ outPrice.textContent = currentPrice * currentDelivery * currentQuantity
+};
